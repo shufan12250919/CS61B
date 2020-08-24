4c89b87fd18e8b91a00345fbc81a9e59614b4cbe
linshu-fan <linshuf@oregonstate.edu>
2020-08-24 22:17:29 +0800

    use only one method

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -3,61 +3,56 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestArrayDequeGold {
+
     @Test
-    public void Test() {
-        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> answer = new ArrayDequeSolution<>();
-        String message = "";
-        for (int i = 0; i < 100; i++) {
-            int type = StdRandom.uniform(1, 5);
-            if (type == 1) {
-                removeFirst(student, answer, message);
-            } else if (type == 2) {
-                removeLast(student, answer, message);
-            } else if (type == 3) {
-                addFirst(student, answer, message);
-            } else if (type == 4) {
-                addLast(student, answer, message);
+    public void testSad() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ans = new ArrayDequeSolution<>();
+
+        String output = "";
+
+        int count = 0;
+        while (true) {
+            double randNum = StdRandom.uniform();
+
+            if (randNum < 0.25) {
+                sad.addFirst(count);
+                ans.addFirst(count);
+                // Integer actual = sad.get(0);
+                // Integer expected = ans.get(0);
+                output += "\naddFirst(" + count + ")";
+                // assertEquals(output, expected, actual);
+
+            } else if (randNum < 0.5) {
+                sad.addLast(count);
+                ans.addLast(count);
+                // Integer actual = sad.get(sad.size() - 1);
+                // Integer expected = ans.get(sad.size() - 1);
+                output += "\naddLast(" + count + ")";
+                // assertEquals(output, expected, actual);
+
+            } else if (randNum < 0.75) {
+                Integer actual = sad.removeFirst();
+                Integer expected = ans.removeFirst();
+                if (actual == null || expected == null) {
+                    continue;
+                }
+                output += "\nremoveFirst()";
+                assertEquals(output, expected, actual);
+
+            } else {
+                Integer actual = sad.removeLast();
+                Integer expected = ans.removeLast();
+                if (actual == null || expected == null) {
+                    continue;
+                }
+                output += "\nremoveLast()";
+                assertEquals(output, expected, actual);
             }
+
+            count += 1;
         }
 
-
     }
-
-    private void removeFirst(StudentArrayDeque<Integer> s, ArrayDequeSolution<Integer> a, String m) {
-        Integer expected = a.removeFirst();
-        Integer actual = s.removeFirst();
-        if (expected == null || actual == null) {
-            return;
-        }
-        m = m + "removeFirst()\n";
-        assertEquals(m, expected, actual);
-
-    }
-
-    private void removeLast(StudentArrayDeque<Integer> s, ArrayDequeSolution<Integer> a, String m) {
-        Integer expected = a.removeLast();
-        Integer actual = s.removeLast();
-        if (expected == null || actual == null) {
-            return;
-        }
-        m = m + "removeLast()\n";
-        assertEquals(m, expected, actual);
-    }
-
-    private void addFirst(StudentArrayDeque<Integer> s, ArrayDequeSolution<Integer> a, String m) {
-        int num = StdRandom.uniform(0, 100);
-        s.addFirst(num);
-        a.addFirst(num);
-        m = m + "addFirst(" + num + ")\n";
-    }
-
-    private void addLast(StudentArrayDeque<Integer> s, ArrayDequeSolution<Integer> a, String m) {
-        int num = StdRandom.uniform(0, 100);
-        s.addLast(num);
-        a.addLast(num);
-        m = m + "addLast(" + num + ")\n";
-    }
-
 
 }
