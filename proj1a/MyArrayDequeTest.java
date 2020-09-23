2ada198a3d44d2afb88811cb30e152e898327844
linshu-fan <frankfrank410@gmail.com>
2020-09-23 18:54:29 +0800

    update proj1a

--- proj1a/MyArrayDequeTest.java
+++ proj1a/MyArrayDequeTest.java
@@ -1,12 +1,21 @@
 public class MyArrayDequeTest {
     public static void main(String[] args) {
         System.out.println("Test started!");
-        ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
-        for (int i = 0; i < 9; i++) {
-            deque.addFirst(i);
+        ArrayDeque<Integer> d = new ArrayDeque<>();
+        for (int i = 0; i < 20; i++) {
+            d.addLast(i);
         }
-        deque.printDeque();
-        System.out.println(deque.get(1));
+        d.printDeque();
+        for (int i = 0; i < 100; i++) {
+            d.removeFirst();
+        }
+
+
+        d.printDeque();
+        d.addLast(1);
+        d.printDeque();
+        d.addFirst(2);
+        d.printDeque();
         System.out.println("Test completed!");
     }
 }
--- proj1a/MyLinkedListDequeTest.java
+++ proj1a/MyLinkedListDequeTest.java
@@ -3,13 +3,8 @@ public class MyLinkedListDequeTest {
     public static void main(String[] args) {
         System.out.println("Test started!");
         LinkedListDeque<Integer> d = new LinkedListDeque<Integer>();
-        d.addFirst(0);
-        d.isEmpty();
-        d.addFirst(2);
         d.addFirst(3);
-        d.addFirst(4);
-        d.isEmpty();
-        System.out.println(d.removeLast());
+        System.out.println(d.get(0));
         d.printDeque();
     }
 }
