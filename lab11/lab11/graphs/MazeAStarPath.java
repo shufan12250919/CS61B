6a5dd33bf8c59fa107e2790fa04d74bb0102db07
linshu-fan <frankfrank410@gmail.com>
2020-10-12 19:30:00 +0800

    finish optional task

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,13 +1,32 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.MinPQ;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeAStarPath extends MazeExplorer {
     private int s;
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+
+
+    private class Vertex implements Comparable {
+        private int index;
+        private int estimatedDistance;
+
+        private Vertex(int v, int dist) {
+            index = v;
+            estimatedDistance = h(v) + dist;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Vertex n = (Vertex) o;
+            return this.estimatedDistance - n.estimatedDistance;
+        }
+    }
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -18,20 +37,50 @@ public class MazeAStarPath extends MazeExplorer {
         edgeTo[s] = s;
     }
 
-    /** Estimate of the distance from v to the target. */
+    /**
+     * Estimate of the distance from v to the target.
+     */
     private int h(int v) {
-        return -1;
+        int targetX = maze.toX(t);
+        int targetY = maze.toY(t);
+        int sourceX = maze.toX(v);
+        int sourceY = maze.toY(v);
+        return Math.abs(sourceX - targetX) + Math.abs(sourceY - targetY);
     }
 
-    /** Finds vertex estimated to be closest to target. */
+    /**
+     * Finds vertex estimated to be closest to target.
+     */
     private int findMinimumUnmarked() {
         return -1;
         /* You do not have to use this method. */
     }
 
-    /** Performs an A star search from vertex s. */
+    /**
+     * Performs an A star search from vertex s.
+     */
     private void astar(int s) {
-        // TODO
+        MinPQ<Vertex> priorityQueue = new MinPQ<>();
+        priorityQueue.insert(new Vertex(s, distTo[s]));
+        while (!priorityQueue.isEmpty()) {
+            Vertex next = priorityQueue.delMin();
+            marked[next.index] = true;
+            announce();
+            if (next.index == t) {
+                return;
+            }
+            for (int w : maze.adj(next.index)) {
+                //check the currnt next->w distance is a shorter than old distance to w
+                if (distTo[w] > distTo[next.index] + 1) {
+                    edgeTo[w] = next.index;
+                    distTo[w] = distTo[next.index] + 1;
+                }
+                // marked means already checked the best way
+                if (!marked[w]) {
+                    priorityQueue.insert(new Vertex(w, distTo[w]));
+                }
+            }
+        }
     }
 
     @Override
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,7 @@
 package lab11.graphs;
 
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,16 +9,55 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    // to store information of edgeTo, for not drawing out all the edges
+    private int[] parent;
+    private boolean cycle;
 
     public MazeCycles(Maze m) {
         super(m);
+        parent = new int[marked.length];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        //start from 0, 0
+        parent[0] = 0;
+        dfs(0);
+        announce();
     }
 
     // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+        announce();
+        for (int w : maze.adj(v)) {
+            // avoid searching for another node, after returning from call stack
+            if (cycle) {
+                return;
+            }
+            if (marked[w]) {
+                if (w != parent[v]) {
+                    parent[w] = v; // set the edges become cycle
+                    drawCycle(w);
+                    cycle = true;
+                    return;
+                }
+            } else {
+                parent[w] = v;
+                dfs(w);
+            }
+        }
+    }
+
+    //draw the cycle
+    private void drawCycle(int w) {
+        int v = parent[w];
+        edgeTo[v] = parent[v];
+        while (v != w) {
+            v = parent[v];
+            edgeTo[v] = parent[v];
+        }
+    }
+
 }
 
